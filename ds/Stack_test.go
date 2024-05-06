7c265a3b9462cefbde8464738236b7c8096df980
Colin Faivre <colin.faivre@gmail.com>
2024-05-06 15:15:08 +0200

    refactor: :recycle: use ginkgo on Stack test

--- ds/Stack_test.go
+++ ds/Stack_test.go
@@ -2,18 +2,45 @@ package ds
 
 import (
 	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 func TestStack(t *testing.T) {
-	stack := Stack{}
-	stack.Push(3)
-	stack.Push(2)
-	stack.Push(1)
-	stack.Pop()
-	stack.Pop()
-
-	// fmt.Print("stack ", stack)
-	if stack.items[0] != 3 {
-		t.Errorf("stack first item expected %v but received %v", 3, stack.items[0])
-	}
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Stack Suite")
 }
+
+var _ = Describe("Stack", func() {
+	Context("Initially", func() {
+		stack := Stack{}
+
+		It("Has an empty slice of integers", func() {
+			Expect(stack.IsEmpty()).Should(Equal(true))
+		})
+	})
+
+	Context("When Push() is called", func() {
+		stack := Stack{}
+		stack.Push(42)
+
+		It("adds an item at the end of the stack", func() {
+			Expect(stack.items[len(stack.items)-1]).Should(Equal(42))
+		})
+	})
+
+	Context("When Pop() is called on a one element stack", func() {
+		stack := Stack{}
+		stack.Push(42)
+		removed := stack.Pop()
+
+		It("returns the popped integer", func() {
+			Expect(removed).Should(Equal(42))
+		})
+
+		It("should be empty", func() {
+			Expect(stack.IsEmpty()).Should(Equal(true))
+		})
+	})
+})
