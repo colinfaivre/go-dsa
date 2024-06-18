47e115e507dc7507ba88a0e041421d58c4a028f2
Colin Faivre <colin.faivre@gmail.com>
2024-06-18 17:13:14 +0200

    docs: :memo: finish documenting training problems

--- training/8_heap_priority_queues/4_kth_largest_element_in_an_array.go
+++ training/8_heap_priority_queues/4_kth_largest_element_in_an_array.go
@@ -3,6 +3,21 @@ package training
 // 8.4 `M` Kth Largest Element In An Array
 
 /*** @LEETCODE https://leetcode.com/problems/kth-largest-element-in-an-array/
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Can you solve it without sorting?
+---
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+---
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+---
+Constraints:
+1 <= k <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
 ***/
 
 /*** @SOLUTION https://www.youtube.com/watch?v=XEmy13g1Qxc
