d54fe367c66140c978af709a50e36300b4daa9a2
Colin Faivre <colin.faivre@gmail.com>
2024-06-18 15:33:26 +0200

    feat: prepare johnson's algorithm implementation

---
+++ problems/johnson.go
@@ -0,0 +1,24 @@
+package problems
+
+import "github.com/colinfaivre/go-dsa/datastructures"
+
+func Johnson(g *datastructures.Graph) {
+	// #1 O(n)		form G' by adding a new vertex s and a new edge (s, v)
+	// 				with weight 0 for each v of G
+
+	// #2 O(mn)		run BellMan-Ford on G' from s
+	// 				if B-F detects a negative cost cycle in G', halt and report
+
+	// #3 O(m)		for each v of G, define:
+	//				Pv := length of a shortest (s, v) path in G'
+	//				for each edge e = (u, v) of G, define:
+	//				Ce' := Ce + Pu - Pv
+
+	// #4 O(nmlogn)	for each u of G:
+	// 				run Dijkstra in G with edge lengths Ce', from vertex u
+	// 				to compute the shortest-path distance d'(u, v) for each v of G
+
+	// #5 O(n^2)	for each pair (u, v) of G
+	//				return the shortest-path distance:
+	//				d(u, v) := d'(u, v) - Pu + Pv
+}
---
+++ problems/johnson_test.go
@@ -0,0 +1,40 @@
+package problems_test
+
+import (
+	"github.com/colinfaivre/go-dsa/datastructures"
+	"github.com/colinfaivre/go-dsa/parsing"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Johnson", func() {
+	Context("Huge graph 1", func() {
+		It("computes the assignement right clustering", func() {
+			huge_graph := datastructures.NewGraph(true)
+			adj_list, _ := parsing.ReadIntegers3TuplesFromFile("../test/data/g1")
+			huge_graph.Add3TuplesEdges(adj_list)
+
+			Expect(len(huge_graph.GetVertices())).To(Equal(1000))
+		})
+	})
+
+	Context("Huge graph 2", func() {
+		It("computes the assignement right clustering", func() {
+			huge_graph := datastructures.NewGraph(true)
+			adj_list, _ := parsing.ReadIntegers3TuplesFromFile("../test/data/g2")
+			huge_graph.Add3TuplesEdges(adj_list)
+
+			Expect(len(huge_graph.GetVertices())).To(Equal(1000))
+		})
+	})
+
+	Context("Huge graph 3", func() {
+		It("computes the assignement right clustering", func() {
+			huge_graph := datastructures.NewGraph(true)
+			adj_list, _ := parsing.ReadIntegers3TuplesFromFile("../test/data/g3")
+			huge_graph.Add3TuplesEdges(adj_list)
+
+			Expect(len(huge_graph.GetVertices())).To(Equal(1000))
+		})
+	})
+})
