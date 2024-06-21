12ee11e3f43b5827f1fee8e43257167d2906e48a
Colin Faivre <colin.faivre@gmail.com>
2024-06-21 08:52:20 +0200

    Update 2_min_stack.go

--- training/3_stack/2_min_stack.go
+++ training/3_stack/2_min_stack.go
@@ -35,4 +35,38 @@ At most 3 * 104^ calls will be made to push, pop, top, and getMin.
 ***/
 
 /*** @SOLUTION https://www.youtube.com/watch?v=qkLl7nAwDPo
+O(1) solution - two stacks
+a stack for items and another for minimums
+update the two stacks when pushing and popping
 ***/
+
+type MinStack struct {
+    items    []int
+    minimums []int   
+}
+
+func Constructor() MinStack {
+    return MinStack{items: []int{}, minimums: []int{}}
+}
+
+func (this *MinStack) Push(val int)  {
+    this.items = append(this.items, val)
+    minValue := val
+    if len(this.minimums) != 0 {
+        minValue = this.minimums[len(this.minimums) - 1]
+    }
+    this.minimums = append(this.minimums, min(minValue, val))
+}
+
+func (this *MinStack) Pop()  {
+    this.minimums = this.minimums[:len(this.minimums)-1]
+    this.items = this.items[:len(this.items)-1]
+}
+
+func (this *MinStack) Top() int {
+    return this.items[len(this.items) - 1]
+}
+
+func (this *MinStack) GetMin() int {
+    return this.minimums[len(this.minimums)-1]
+}
