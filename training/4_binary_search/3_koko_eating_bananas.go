5ae90875224a97b57308015fa4fcaf8a267a6797
Colin Faivre <colin.faivre@gmail.com>
2024-07-02 14:54:05 +0200

    feat: solve koko eating bananas

--- training/4_binary_search/3_koko_eating_bananas.go
+++ training/4_binary_search/3_koko_eating_bananas.go
@@ -29,4 +29,48 @@ piles.length <= h <= 10^9
 ***/
 
 /*** @SOLUTION https://www.youtube.com/watch?v=U2SozAs9RzA
+O(max*n) solution: brute force
+- init max to max value of piles
+- loop with k from 1 to max
+  - init hours to 0
+  - loop in piles incrementing hours by (current pile / k)
+  - if hours is less or equal to h
+    - set res to min of res and k
+- return res
+
+O(log(max)*n) solution: binary search
+- init left and right pointers to 1 and the max value of piles
+- init res to right value
+- loop while left is less or equal to right
+  - init k (eating speed) to middle value from left to right
+  - init hours to 0
+  - loop in piles incrementing hours by (current pile / k)
+  - if hours is less or equal to h
+    - set res to min of res and k
+    - set right pointer to k - 1
+  - else set left pointer to k + 1
+- return res
 ***/
+
+func MinEatingSpeed(piles []int, h int) int {
+    l, r := 1, slices.Max(piles)
+    res := r
+
+    for l <= r {
+        k := (l + r) / 2
+        hours := 0
+
+        for _, p := range piles {
+            hours += int(math.Ceil(float64(p) / float64(k)))
+        }
+
+        if hours <= h {
+            res = min(res, k)
+            r = k - 1
+        } else {
+            l = k + 1
+        }
+    }
+
+    return res
+}
