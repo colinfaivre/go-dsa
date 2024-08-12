ed795b4cec0fe994c2b17d278e9c81dd985c3883
Colin Faivre <colin.faivre@gmail.com>
2024-08-12 09:36:52 +0200

    Update 4_permutation_in_string.go

--- training/5_sliding_window/4_permutation_in_string.go
+++ training/5_sliding_window/4_permutation_in_string.go
@@ -21,4 +21,43 @@ s1 and s2 consist of lowercase English letters.
 ***/
 
 /*** @SOLUTION https://www.youtube.com/watch?v=UbyhOgBN834
+O(26n) solution:
+- return false if s1 is bigger than s2
+- init cnt1 and cnt2 as two 26 sized arrays mapping alphabet letters to their count
+- loop through s1 to fill cnt1 with its char count and cnt2 with s2 subset of s1 length char count
+- return true if cnt1 == cnt2
+- loop from s1 length to s2 length with i
+  - increment cnt2 at s2[i]
+  - decrement cnt2 at s2[i- s1 length]
+  - return true if cnt1 == cnt2
+- return false
 ***/
+
+func CheckInclusion(s1 string, s2 string) bool {
+    l1, l2 := len(s1), len(s2)
+
+	if l1 > l2 {
+		return false
+	}
+
+	var cnt1, cnt2 [26]int
+
+	for i, ch := range s1 {
+		cnt1[ch-'a']++
+		cnt2[s2[i]-'a']++
+	}
+
+	if cnt1 == cnt2 {
+		return true
+	}
+    
+	for i := l1; i < l2; i++ {
+		cnt2[s2[i]-'a']++
+		cnt2[s2[i-l1]-'a']--
+		if cnt1 == cnt2 {
+			return true
+		}
+	}
+
+	return false
+}
