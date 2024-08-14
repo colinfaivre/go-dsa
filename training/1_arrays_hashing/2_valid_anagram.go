f7f46e0a003fb1b76e943dc1db165f721abad95c
Colin Faivre <colin.faivre@gmail.com>
2024-08-14 10:13:26 +0200

    Update 2_valid_anagram.go

--- training/1_arrays_hashing/2_valid_anagram.go
+++ training/1_arrays_hashing/2_valid_anagram.go
@@ -20,4 +20,40 @@ s and t consist of lowercase English letters.
 ***/
 
 /*** @SOLUTION https://www.youtube.com/watch?v=9UtInBqnCgA
+sort solution: O(nlogn) - Space O(1)
+- sort s
+- sort t
+- return s == t
+
+hashmap solution: O(n) - Space O(n)
+- return false if s and t have != length
+- init countS, countT as hashmaps mapping chars to their count
+- loop in s with i
+  - increment countS at s[i]
+  - increment countT at t[i]
+- loop in countS with key
+  - return false if countS at key != countT at key
+- return true
 ***/
+
+func IsAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    countS := map[byte] int {}
+    countT := map[byte] int {}
+
+    for i := range s {
+        countS[s[i]]++
+        countT[t[i]]++
+    }
+
+    for k := range countS {
+        if countS[k] != countT[k] {
+            return false
+        }
+    }
+
+    return true
+}
