f6bb95ecd4c5fa51027c35d3f32db1543f7eb6f2
Colin Faivre <colin.faivre@gmail.com>
2024-09-23 11:37:56 +0200

    Update 2_min_stack.go

--- training/3_stack/2_min_stack.go
+++ training/3_stack/2_min_stack.go
@@ -41,32 +41,32 @@ update the two stacks when pushing and popping
 ***/
 
 type MinStack struct {
-	items    []int
-	minimums []int
+    items []int
+    minimums []int
 }
 
 func Constructor() MinStack {
-	return MinStack{items: []int{}, minimums: []int{}}
+    return MinStack{}
 }
 
-func (s *MinStack) Push(val int) {
-	s.items = append(s.items, val)
-	minValue := val
-	if len(s.minimums) != 0 {
-		minValue = s.minimums[len(s.minimums)-1]
-	}
-	s.minimums = append(s.minimums, min(minValue, val))
+func (this *MinStack) Push(val int)  {
+    this.items = append(this.items, val)
+    minValue := val
+    if len(this.minimums) != 0 {
+        minValue = this.GetMin()
+    }
+    this.minimums = append(this.minimums, min(val, minValue))
 }
 
-func (s *MinStack) Pop() {
-	s.minimums = s.minimums[:len(s.minimums)-1]
-	s.items = s.items[:len(s.items)-1]
+func (this *MinStack) Pop()  {
+    this.items = this.items[:len(this.items) - 1]
+    this.minimums = this.minimums[:len(this.minimums) - 1]
 }
 
-func (s *MinStack) Top() int {
-	return s.items[len(s.items)-1]
+func (this *MinStack) Top() int {
+    return this.items[len(this.items) - 1]
 }
 
-func (s *MinStack) GetMin() int {
-	return s.minimums[len(s.minimums)-1]
+func (this *MinStack) GetMin() int {
+    return this.minimums[len(this.minimums) - 1]
 }
