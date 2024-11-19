7bbc2f4739b30d1540918950cac6ae97fab48872
Colin Faivre <colin.faivre@gmail.com>
2024-11-19 09:03:50 +0100

    Update 11_validate_binary_search_tree.go

--- training/7_trees/11_validate_binary_search_tree.go
+++ training/7_trees/11_validate_binary_search_tree.go
@@ -27,3 +27,24 @@ The number of nodes in the tree is in the range [1, 10^4].
 
 /*** @SOLUTION https://www.youtube.com/watch?v=s6ATEkipzow
 ***/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+    return validBST(root, math.MinInt, math.MaxInt)
+}
+
+func validBST(root *TreeNode, left, right int) bool {
+    if root == nil { return true }
+    if root.Val >= right || root.Val <= left { return false }
+
+    isValidLeft := validBST(root.Left, left, root.Val)
+    isValidRight := validBST(root.Right, root.Val, right)
+    return isValidLeft && isValidRight
+}
