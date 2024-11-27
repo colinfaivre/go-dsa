3effdf38d332eedcd2a2d8039e729bbc27bb5f7e
Colin Faivre <colin.faivre@gmail.com>
2024-11-27 08:33:15 +0100

    Update 4_kth_largest_element_in_an_array.go

--- training/8_heap_priority_queues/4_kth_largest_element_in_an_array.go
+++ training/8_heap_priority_queues/4_kth_largest_element_in_an_array.go
@@ -22,3 +22,34 @@ Constraints:
 
 /*** @SOLUTION https://www.youtube.com/watch?v=XEmy13g1Qxc
 ***/
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, k int) int {
+    numsMaxHeap := &MaxHeap{}
+    heap.Init(numsMaxHeap)
+    var res int
+    for _, val := range nums {
+        heap.Push(numsMaxHeap, val)
+    }
+    for i := 1; i <= k; i++ {
+        res = heap.Pop(numsMaxHeap).(int)
+    }
+    return res
+}
