d1df164183a8f4007654e59cea3752ab085a5a0e
Colin Faivre <colin.faivre@gmail.com>
2024-12-02 10:26:46 +0100

    Update 1_number_of_islands.go

--- training/9_graphs/1_number_of_islands.go
+++ training/9_graphs/1_number_of_islands.go
@@ -35,3 +35,39 @@ grid[i][j] is '0' or '1'.
 
 /*** @SOLUTION https://www.youtube.com/watch?v=pV2kpPD66nE
 ***/
+
+func dfs(grid [][]byte, r, c int) {
+    rows := len(grid)
+    cols := len(grid[0])
+    if r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0' {
+        return // Base case: If out of bounds or on water ('0'), return
+    }
+
+    grid[r][c] = '0'  // Mark the cell as visited by setting it to '0'
+
+    dfs(grid, r+1, c) // Down
+    dfs(grid, r-1, c) // Up
+    dfs(grid, r, c+1) // Right
+    dfs(grid, r, c-1) // Left
+}
+
+func numIslands(grid [][]byte) int {
+    if len(grid) == 0 || len(grid[0]) == 0 {
+        return 0
+    }
+
+    rows := len(grid)
+    cols := len(grid[0])
+    count := 0
+
+    for r := 0; r < rows; r++ {
+        for c := 0; c < cols; c++ {
+            if grid[r][c] == '1' {
+                count++
+                dfs(grid, r, c) // Start DFS to mark the entire island
+            }
+        }
+    }
+
+    return count
+}
